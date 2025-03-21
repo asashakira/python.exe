ca0ddec05a1c7f9a411f1f0ef4db22e0706f507f
akira <asa.shakira340@gmail.com>
2025-03-21 12:55:42 +0900

    read from config.yaml

---
+++ pythonexe/main.py
@@ -0,0 +1,31 @@
+import yaml
+from pathlib import Path
+
+def read_config(file_path: str) -> dict[str, str | None] | None:
+    """Read configs from a YAML file."""
+    try:
+        with Path(file_path).open("r", encoding="utf-8") as file:
+            data = yaml.safe_load(file)
+            return {
+                "custid": data.get("custid"),
+                "apikey": data.get("apikey"),
+                "host": data.get("host"),
+                "username": data.get("username"),
+                "password": data.get("password"),
+                "slack_webhook_url": data.get("slack_webhook_url"),
+                "remote_path": data.get("remote_path"),
+            }
+    except FileNotFoundError:
+        print("Error: File '%s' not found.", file_path)
+        return None
+
+    except yaml.YAMLError:
+        print("Error parsing YAML")
+        return None
+
+def main():
+    config = read_config("config.yaml")
+    print(config)
+
+if __name__ == "__main__":
+    main()
